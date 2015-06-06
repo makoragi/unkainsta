1816e66d9210bbde1684891ce5c76e12ff79783c
makoragi <makoragi@gmail.com>
2015-06-07 00:34:39 +0900

    スライドショー(least)を利用

--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,10 @@ router.get('/', function(req, res, next) {
 	var baseurl = 'https://api.instagram.com/v1/tags/%e9%9b%b2%e6%b5%b7/media/recent';
 	request(baseurl + '?access_token=' + process.env.access_token, function(error, response, body){
 		if (error) {
-			console.log(error);
+			// console.log(error);
 		} else {
 			var json = JSON.parse(body);
-			console.log(json.data);
+			// console.log(json.data);
 			res.render('index', {
 				title: 'Unkainsta - 雲海 x Instagram',
 				data: json.data

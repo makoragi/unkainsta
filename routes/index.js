7e5ce483808fb6313e4814c0e1bbef4a336f249b
makoragi <makoragi@gmail.com>
2015-06-06 22:03:04 +0900

    とりあえず雲海ハッシュタグ検索結果を表示

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+
+var posts = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	// var baseurl = 'https://api.instagram.com/v1/tags/kikakuma/media/recent';
+	// var baseurl = 'https://api.instagram.com/v1/tags/雲海/media/recent';
+	var baseurl = 'https://api.instagram.com/v1/tags/%e9%9b%b2%e6%b5%b7/media/recent';
+	request(baseurl + '?access_token=' + process.env.access_token, function(error, response, body){
+		if (error) {
+			console.log(error);
+		} else {
+			var json = JSON.parse(body);
+			console.log(json.data);
+			res.render('index', {
+				title: 'Unkainsta - 雲海 x Instagram',
+				data: json.data
+			});
+		}
+	});
+
 });
 
 module.exports = router;

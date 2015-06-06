415ad18a134bc589b17633d46ad908a173933d07
makoragi <makoragi@gmail.com>
2015-06-07 02:01:19 +0900

    タグ検索をラピュタの道にした

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ var posts = [];
 router.get('/', function(req, res, next) {
 	// var baseurl = 'https://api.instagram.com/v1/tags/kikakuma/media/recent';
 	// var baseurl = 'https://api.instagram.com/v1/tags/雲海/media/recent';
-	var baseurl = 'https://api.instagram.com/v1/tags/%e9%9b%b2%e6%b5%b7/media/recent';
+	// var baseurl = 'https://api.instagram.com/v1/tags/%e9%9b%b2%e6%b5%b7/media/recent';
+	var baseurl = 'https://api.instagram.com/v1/tags/%e3%83%a9%e3%83%94%e3%83%a5%e3%82%bf%e3%81%ae%e9%81%93/media/recent';
 	request(baseurl + '?access_token=' + process.env.access_token, function(error, response, body){
 		if (error) {
 			// console.log(error);

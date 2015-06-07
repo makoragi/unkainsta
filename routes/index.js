8fbeec662e89153964208ca795eb75c3e58b8d8e
makoragi <makoragi@gmail.com>
2015-06-07 11:03:19 +0900

    pagination追加

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,42 @@ var request = require('request');
 
 var posts = [];
 
+// var baseurl = 'https://api.instagram.com/v1/tags/kikakuma/media/recent';
+// var baseurl = 'https://api.instagram.com/v1/tags/雲海/media/recent';
+// var baseurl = 'https://api.instagram.com/v1/tags/%e9%9b%b2%e6%b5%b7/media/recent';
+var baseurl = 'https://api.instagram.com/v1/tags/%e3%83%a9%e3%83%94%e3%83%a5%e3%82%bf%e3%81%ae%e9%81%93/media/recent';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	// var baseurl = 'https://api.instagram.com/v1/tags/kikakuma/media/recent';
-	// var baseurl = 'https://api.instagram.com/v1/tags/雲海/media/recent';
-	// var baseurl = 'https://api.instagram.com/v1/tags/%e9%9b%b2%e6%b5%b7/media/recent';
-	var baseurl = 'https://api.instagram.com/v1/tags/%e3%83%a9%e3%83%94%e3%83%a5%e3%82%bf%e3%81%ae%e9%81%93/media/recent';
 	request(baseurl + '?access_token=' + process.env.access_token, function(error, response, body){
 		if (error) {
 			// console.log(error);
 		} else {
 			var json = JSON.parse(body);
-			// console.log(json.data);
+			// console.log(json);
 			res.render('index', {
 				title: 'Unkainsta - 雲海 x Instagram',
-				data: json.data
+				json: json
 			});
 		}
 	});
 
+});
+
+router.post('/', function(req, res){
+	// console.log(req.body);
+	request(baseurl + '?access_token=' + process.env.access_token + '&max_tag_id=' + req.body.max_tag_id, function(error, response, body){
+		if (error) {
+			// console.log(error);
+		} else {
+			var json = JSON.parse(body);
+			// console.log(json);
+			res.render('index', {
+				title: 'Unkainsta - 雲海 x Instagram',
+				json: json
+			});
+		}
+	});
 });
 
 module.exports = router;
